3894b870bca3843a8e27ea6fa50202866e004627
Cristian Zavaleta <cristian.zavaleta.b@tecsup.edu.pe>
2025-11-21 14:17:15 -0500

    Fix User model import in views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets, generics, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+User = get_user_model()
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
