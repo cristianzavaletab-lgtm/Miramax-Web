0700971c0a0bd77747e11ab3a07cfd97302c2f91
Cristian Zavaleta <cristian.zavaleta.b@tecsup.edu.pe>
2025-11-21 14:21:16 -0500

    Fix missing imports in payments/views.py

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,4 +1,12 @@
-
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
+from django.http import FileResponse
+from .models import Payment, MonthlyFee, Service, ConfigPreciosZona
+from .serializers import PaymentSerializer, MonthlyFeeSerializer, ServiceSerializer, ConfigPreciosZonaSerializer
+from .utils import generate_payment_receipt
 
 class ConfigPreciosZonaViewSet(viewsets.ModelViewSet):
     """
