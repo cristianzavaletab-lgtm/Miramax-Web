281f3201c1f3307c151901557df88440406e1d62
Cristian Zavaleta <cristian.zavaleta.b@tecsup.edu.pe>
2025-11-22 10:25:57 -0500

    Update create_superuser.py to use env vars

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -6,8 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-    print('Superuser created')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+if not User.objects.filter(username=username).exists():
+    print(f'Creating superuser {username}...')
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created successfully')
 else:
-    print('Superuser already exists')
+    print(f'Superuser {username} already exists')
